d6ab8c978d9c9839572dafd0d870a4beb1a96351
Santiago González Siordia <santiago.gonzalez.siordia@gmail.com>
2020-08-19 18:33:52 -0500

    Fixed?

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 app.get('/', (req, res) => {
-   res.send({ hi: "there" });
+   res.send({ hi: 'there' });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
